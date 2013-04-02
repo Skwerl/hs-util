ace81f1beff69714ac1f113fa36bb63c6f0f6b88
Kevin Cogill <skwerl@gmail.com>
2013-04-02 18:31:11 -0400

    issue #13 ccd import allergy status

--- cmts/ccd_deserialize.php
+++ cmts/ccd_deserialize.php
@@ -84,6 +84,7 @@ $allergiesIndex = 0;
 foreach ($allergies as $allergy) {
 	$obj['allergy'][] = array(
 		'name' => s($allergiesTable[$allergiesIndex]->td[1]),
+		'active' => (strtolower(s($allergiesTable[$allergiesIndex]->td[3])) == 'active' ? true : false),
 		'allergicReaction' => s($allergiesTable[$allergiesIndex]->td[2]),
 		'allergicReactionDate' => date('Y-m-d',strtotime(s($allergy->act->entryRelationship->observation->effectiveTime->low['value']))),
 		'snomed' => s($allergy->act->entryRelationship->observation->value['code'])
